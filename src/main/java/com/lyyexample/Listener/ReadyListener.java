9578b5ac2b78c4e9aeb6b6ee0344aa3c624d521e
yeshen2013 <1486258506@qq.com>
2018-11-06 21:33:33 +0800

    添加listener

---
+++ src/main/java/com/lyyexample/Listener/ReadyListener.java
@@ -0,0 +1,18 @@
+package com.lyyexample.Listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by liuyangyang on 2018/11/6.
+ */
+@Slf4j
+//@Component
+public class ReadyListener implements ApplicationListener<ApplicationReadyEvent> {
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+        log.info("&&&&&&&&&&&&&&&&&&&&&&&进入ReadyListener");
+    }
+}
--- src/main/java/com/lyyexample/interceptor/LoginInterceptor.java
+++ src/main/java/com/lyyexample/interceptor/LoginInterceptor.java
@@ -1,5 +1,7 @@
 package com.lyyexample.interceptor;
 
+import com.alibaba.fastjson.JSONObject;
+import com.lyyexample.common.ResponseModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -17,16 +19,32 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
         logger.info("请求进入拦截器！"+httpServletRequest.getMethod());
+        if(o instanceof ResponseModel){
+            ResponseModel responseModel = (ResponseModel)o;
+            responseModel.setRetMsg("请求preHandle");
+            logger.info(JSONObject.toJSONString(o));
+        }
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
-        logger.info("请求处理过程！");
+        logger.info("进入请求处理过程！");
+        if(o instanceof ResponseModel){
+            ResponseModel responseModel = (ResponseModel)o;
+            responseModel.setRetMsg("请求处理过程");
+            logger.info(JSONObject.toJSONString(o));
+        }
     }
 
     @Override
     public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        logger.info("完成请求："+o.toString());
+        if(o instanceof ResponseModel){
+            ResponseModel responseModel = (ResponseModel)o;
+            responseModel.setRetMsg("请求正确返回");
+            logger.info(JSONObject.toJSONString(o));
+        }
         logger.info("请求正确返回！"+HttpServletResponse.SC_OK);
     }
 }
